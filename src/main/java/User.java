6f6fa00a24f18ee7b27c6c9910ef29b751e116c9
Thiago Machado <consultoria.tmachado@grupoivory.com.br>
2022-02-10 23:51:51 -0300

    Initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,10 @@
+public class User {
+
+    private String nome;
+    private Integer saldo;
+
+    public User(String nome, Integer saldo) {
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+}
