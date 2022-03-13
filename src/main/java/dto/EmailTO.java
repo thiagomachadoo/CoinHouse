775dfd0dab3add280e229d4985b8295b1b09d94c
Thiago Machado <consultoria.tmachado@grupoivory.com.br>
2022-03-12 22:13:26 -0300

    regras de email

--- src/main/java/dto/EmailTO.java
+++ src/main/java/dto/EmailTO.java
@@ -4,6 +4,21 @@ public class EmailTO {
     private String enderecoEmail;
 
     public EmailTO(String enderecoEmail) {
+        if (enderecoEmail == null || !enderecoEmail.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")){
+            throw new IllegalArgumentException("E-mail inválido");
+        }else{
+            System.out.println("Email válido!!");
+        }
         this.enderecoEmail = enderecoEmail;
     }
+
+    public String getEnderecoEmail() {
+        return enderecoEmail;
+    }
+
+    public void setEnderecoEmail(String enderecoEmail) {
+        this.enderecoEmail = enderecoEmail;
+    }
+}
 }
